1fd1055aa7c4f9e1851726209f905642810c01e2
Loukas Peteinaris <loukas.peteinaris@insurancemarket.gr>
2020-09-24 01:05:11 +0300

    Override Lexik JWT Auth success to include user's role

---
+++ back-end/src/Service/JWT/AuthenticationSuccessListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service\JWT;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['role'] = $user->getRole();
+
+        $event->setData($data);
+    }
+}
