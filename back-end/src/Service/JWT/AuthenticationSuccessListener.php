f5e44c9c7c503d71439e00a0c38130594e21eca4
Loukas Peteinaris <loukas.peteinaris@insurancemarket.gr>
2020-09-24 02:10:38 +0300

    Added all User info on jwt authentication success

--- back-end/src/Service/JWT/AuthenticationSuccessListener.php
+++ back-end/src/Service/JWT/AuthenticationSuccessListener.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\JWT;
 
+use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -10,13 +11,14 @@ class AuthenticationSuccessListener
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
     {
         $data = $event->getData();
+        /** @var User $user */
         $user = $event->getUser();
 
         if (!$user instanceof UserInterface) {
             return;
         }
 
-        $data['role'] = $user->getRole();
+        $data['userData'] = $user->jsonSerialize();
 
         $event->setData($data);
     }
