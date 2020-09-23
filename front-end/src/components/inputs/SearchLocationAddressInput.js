18c34b376e7588bf0ff44b5b224a530189778555
Loukas Peteinaris <loukas.peteinaris@insurancemarket.gr>
2020-09-24 02:12:02 +0300

    Added Google Autocomplete Inputs

---
+++ front-end/src/components/inputs/SearchLocationAddressInput.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from "react";
+import "../../scss/components/inputs/SearchLocationInput.scss";
+
+let autoComplete;
+
+const loadScript = (url, callback) => {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+
+  if (script.readyState) {
+    script.onreadystatechange = function () {
+      if (script.readyState === "loaded" || script.readyState === "complete") {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {
+    script.onload = () => callback();
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+};
+
+function handleScriptLoad(
+  updateQuery,
+  autoCompleteRef,
+  setAreaAndLocalityFromGoogleAutocomplete
+) {
+  autoComplete = new window.google.maps.places.Autocomplete(
+    autoCompleteRef.current,
+    { types: ["address"] }
+  );
+  autoComplete.setFields(["address_components", "formatted_address"]);
+  autoComplete.addListener("place_changed", () =>
+    handlePlaceSelect(updateQuery, setAreaAndLocalityFromGoogleAutocomplete)
+  );
+}
+
+async function handlePlaceSelect(
+  updateQuery,
+  setAreaAndLocalityFromGoogleAutocomplete
+) {
+  const addressObject = autoComplete.getPlace();
+  const query = addressObject.formatted_address;
+  updateQuery(query);
+  setAreaAndLocalityFromGoogleAutocomplete(
+    addressObject.address_components[2].long_name,
+    addressObject.address_components[3].long_name
+  );
+}
+
+function SearchLocationAddressInput(props) {
+  const [query, setQuery] = useState("");
+  const autoCompleteRef = useRef(null);
+
+  useEffect(() => {
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`,
+      () => handleScriptLoad(setQuery, autoCompleteRef, props.onChange)
+    );
+  }, []);
+
+  return (
+    <div className="search-location-input">
+      <input
+        ref={autoCompleteRef}
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Enter the room's address"
+        value={query}
+      />
+    </div>
+  );
+}
+
+export default SearchLocationAddressInput;
