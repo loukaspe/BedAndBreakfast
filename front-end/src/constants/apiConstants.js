42ff541ac38821faf33f83248cfa6c5c33179a50
Loukas Peteinaris <loukas.peteinaris@insurancemarket.gr>
2020-09-24 02:11:30 +0300

    Added Constants files

---
+++ front-end/src/constants/apiConstants.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "http://127.0.0.1";
+export const API_REGISTER_ROUTE = "/register";
+export const API_LOGIN_ROUTE = "/api/login_check";
+export const API_USERS_ROUTE = "/api/users";
+export const API_ROOMS_ROUTE = "/api/rooms";
+export const API_REVIEWS_ROUTE = "/api/reviews";
+export const API_RESERVATIONS_ROUTE = "/api/reservations";
---
+++ front-end/src/constants/enumsConstants.js
@@ -0,0 +1,11 @@
+export const ADMIN_ROLE = "admin";
+export const HOST_ROLE = "host";
+export const GUEST_ROLE = "guest";
+export const BOTH_ROLE = "both";
+
+export const HOUSE_ROOM_TYPE = "house";
+export const APPARTMENT_ROOM_TYPE = "appartment";
+export const MAISONETTE_ROOM_TYPE = "maisonette";
+export const GUEST_ROOM_ROOM_TYPE = "guestRoom";
+export const HOTEL_ROOM_TYPE = "hotel";
+export const CHALET_ROOM_TYPE = "chalet";
