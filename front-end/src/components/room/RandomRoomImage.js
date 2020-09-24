bd469f86e8da580535425ef567f303310fb82d99
Loukas Peteinaris <loukas.peteinaris@insurancemarket.gr>
2020-09-25 02:38:42 +0300

    Added Random Images Components

---
+++ front-end/src/components/room/RandomRoomImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RandomRoomImage = () => {
+  return (
+    <div>
+      <img
+        src="https://source.unsplash.com/1600x900/?living_room"
+        alt="ROOM PICTURE"
+      />
+    </div>
+  );
+};
+
+export default RandomRoomImage;
---
+++ front-end/src/components/user/RandomProfileImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RandomProfileImage = () => {
+  return (
+    <div>
+      <img
+        src="https://source.unsplash.com/1600x900/?face"
+        alt="PROFILE PICTURE"
+      />
+    </div>
+  );
+};
+
+export default RandomProfileImage;
